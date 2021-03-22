e9e8a2c22313a4cf04cadc532d5ad1d5c342df9d
kaiiam <kblumberg@email.arizona.edu>
2021-03-22 09:50:41 +0100

    change helper fn to preprocess input unit list with prefix and exp

--- qname_processing/SI_parser.py
+++ qname_processing/SI_parser.py
@@ -171,9 +171,9 @@ def flatten(x):
     """
     https://stackoverflow.com/questions/42102473/parsing-values-from-a-list-of-dictionaries-with-nested-list-in-python
     """
-    if isinstance(x, dict) :
+    if isinstance(x, dict):
         return [x]
-    elif isinstance(x, collections.abc.Iterable) :
+    elif isinstance(x, collections.abc.Iterable):
         return [a for i in x for a in flatten(i)]
     else:
         return [x]
@@ -196,30 +196,40 @@ def get_value(key, dict):
 
 
 # --------------------------------------------------
-def replace_slash(result, dict_list):
+def pre_process_unit_list(result, original, dict_list):
     """
-    Fix to get prefixes e.g. "aA/aB/aC"
+    Removes operators "." or "/" to get this back `'operator': '.',`
+    Deals with start = "/" special case
+    Writes out all terms with prefixes (empty string if none exist)
+    Write out all terms with exponents (including 1 if none exist)
     """
-    #print(result)
+    # Get prefix if existing:
+    if "prefix" in result:
+        prefix = result['prefix']
+    else:
+        prefix = ''
+
     if get_value(key="operator", dict=result) == "/":
-        # print(r)
         # if it doesn't have an exponent key create one at -1 else change exp to -
         if "exponent" not in result:
-            # print('no exp', r)
-            # Make a new dict
-            x = {'operator': '.', 'type': result['type'], 'unit': result['unit'], 'exponent': int('-1')}
-            #print(x)
+            x = {'prefix': prefix, 'type': result['type'], 'unit': result['unit'], 'exponent': int('-1')}
             dict_list.append(x)
         else:
-            # print('exp case', r)
             exp = int('-' + str(result['exponent']))
-            x = {'operator': '.', 'type': result['type'], 'unit': result['unit'], 'exponent': exp}
-            #print(x)
+            x = {'prefix': prefix, 'type': result['type'], 'unit': result['unit'], 'exponent': exp}
             dict_list.append(x)
     # no operator or . case
     else:
-        #print(result)  # don't change just append
-        dict_list.append(result)
+        # Get or create exponent
+        if "exponent" in result:
+            exponent = result['exponent']
+        elif original[0] == '/':
+            exponent = -1
+        else:
+            exponent = 1
+        x = {'prefix': prefix, 'type': result['type'], 'unit': result['unit'], 'exponent': exponent}
+        dict_list.append(x)
+
     return dict_list
 
 
@@ -238,22 +248,23 @@ def main():
     # test_list = ["m.s-1"]
     # test_list = ["A.N-1.W-1"]
     # test_list = ["A.B-1.C-1.d-1.eV-1"]
-    # test_list = ["A/B/C/d"]
-    test_list = ["aA/aB/aC"]
+    # test_list = ["A2/B/C/d"]
+    # test_list = ["aA/aB/aC"]
+    test_list = ["/ng/L"]
 
     # # breakup input list one term at a time
     for u in test_list:
         tree = si_grammar.parse(u)
         result = transformer().transform(tree)
         res_flat = flatten(result)
-        # print(u, res_flat)
+        #print(u, res_flat)
 
         new_dict_list = []
         # Function to convert the x/y operator to the x.y-1 style
         for r in res_flat:
             #print(r)
-            replace_slash(r, new_dict_list)
-        print(new_dict_list)
+            pre_process_unit_list(r, u, new_dict_list)
+        print(u, new_dict_list)
 
 
 
